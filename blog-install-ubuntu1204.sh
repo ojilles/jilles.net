cd1a95ba21132875026fe4d5066ef53012aebadf
Jilles Oldenbeuving <ojilles@gmail.com>
2014-05-22 23:36:33 +0000

    script to quickly provision a vm (ubuntu 12.04 tested) to run all this in

---
+++ blog-install-ubuntu1204.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+sudo apt-get -y update
+sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev ncftp git apache2 tidy
+cd /tmp
+wget http://cache.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p353.tar.gz
+tar -xvzf ruby-2.0.0-p353.tar.gz
+cd ruby-2.0.0-p353/
+./configure --prefix=/usr/local
+make
+sudo make install
+sudo gem install jekyll
+sudo gem install therubyracer
+sudo gem install rouge
