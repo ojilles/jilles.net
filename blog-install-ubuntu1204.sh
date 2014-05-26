75e0dd3c9cbe6aec0d6e01dbf39caf7ec41742ec
Jilles Oldenbeuving <ojilles@gmail.com>
2014-05-26 18:12:38 +0000

    made apache configuration automatic as well. hosting on port 4000 so that in vagrant we can open the same port up: that was in your host OS browser it works, but also from inside the browser with the same URLs, making testing much easier (and cant open up port 80 in vagrant

--- blog-install-ubuntu1204.sh
+++ blog-install-ubuntu1204.sh
@@ -12,6 +12,31 @@ sudo gem install jekyll
 sudo gem install therubyracer
 sudo gem install rouge
 sudo gem install sass
+
+# Make modelines work for VIM
 echo "set modeline" > ~/.vimrc
-sudo echo "NameVirtualHost *:4000
-Listen 4000" >> /etc/apache2/ports.conf
+
+HERE DOC for Apache Virtual Host configuration
+sudo cat << EOFAPACHE > /etc/apache2/sites-available/jilles
+<VirtualHost *:4000>
+  ServerAdmin ojilles@gmail.com
+  DocumentRoot /vagrant/jilles.net/_site
+  <Directory "/vagrant/jilles.net/_site">
+    Options Indexes FollowSymLinks MultiViews 
+    AllowOverride All
+    Order allow,deny
+    Allow from all
+  </Directory>    
+</VirtualHost>
+EOFAPACHE
+# and make it available to serve as well
+sudo ln -s /etc/apache2/sites-available/jilles /etc/apache2/sites-enabled/010-jilles
+
+# Ensure Apache is listening on port 4k as well
+sudo cat << EOFPORT >> /etc/apache2/ports.conf
+# here for jilles.net blog
+NameVirtualHost *:4000
+Listen 4000
+EOFPORT
+
+sudo apachectl restart
